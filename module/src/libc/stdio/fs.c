5c6b80ce1d50c11d193a0fbdee43cf2765a3133a
plenjamin <53486418+benmontycomputer@users.noreply.github.com>
2025-12-18 02:47:05 -0500

    Module: implement errno in fs.c

--- module/src/libc/stdio/fs.c
+++ module/src/libc/stdio/fs.c
@@ -2,12 +2,15 @@
 
 #include "sys/syscall.h"
 #include "stdio.h"
-#include "types.h"
+#include "sys/types.h"
 #include "stdlib.h"
 #include "common.h"
+#include "errno.h"
 
 // TODO: implement errno
+// TODO: set the stream's error indicators in addition to errno
 
+// TODO: make this parse properly
 syscall_open_flags_t parse_mode(const char *mode_str) {
     syscall_open_flags_t flags = 0;
 
@@ -31,12 +34,14 @@ syscall_open_flags_t parse_mode(const char *mode_str) {
 }
 
 FILE *fopen(const char *filename, const char *mode_str) {
-    ssize_t fd_res = syscall_open(filename, parse_mode(mode_str));
+    // TODO: set mode correctly instead of 0777
+    ssize_t fd_res = syscall_open(filename, parse_mode(mode_str), 0777);
 
     if (fd_res >= 0) {
         FILE *file = (FILE *)malloc(sizeof(FILE));
         if (!file) {
             syscall_close((size_t)fd_res);
+            errno = ENOMEM;
             return NULL;
         }
 
@@ -44,14 +49,23 @@ FILE *fopen(const char *filename, const char *mode_str) {
         return file;
     }
 
+    errno = -fd_res;
     return NULL;
 }
 
 int fclose(FILE *stream) {
-    if (!stream) return -1;
+    if (!stream) {
+        errno = EINVAL;
+        return EOF;
+    };
 
-    syscall_close(stream->fd);
+    int res = syscall_close(stream->fd);
     free(stream);
+
+    if (res < 0) {
+        errno = -res;
+        return EOF;
+    }
 
     return 0;
 }
@@ -60,6 +74,7 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream) {
     ssize_t res = syscall_read(stream->fd, ptr, size * nmemb);
     if (res < 0) {
         printf("fread: read error on fd %d, errno %d\n", (int)stream->fd, (int)res);
+        errno = -res;
         return 0;
     }
 
@@ -69,3 +84,19 @@ size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream) {
 size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
 int fseek(FILE *stream, long offset, int whence);
 long ftell(FILE *stream);
+
+// TODO: do we want to buffer this or something? It seems really inefficient to do a syscall for every character.
+int fgetc(FILE *stream) {
+    unsigned char ch;
+    ssize_t res = syscall_read(stream->fd, &ch, 1);
+    if (res < 0) {
+        printf("fgetc: read error on fd %d, errno %d\n", (int)stream->fd, (int)res);
+        errno = -res;
+        return EOF;
+    } else if (res == 0) {
+        // EOF
+        return EOF;
+    }
+
+    return (int)ch;
+}
