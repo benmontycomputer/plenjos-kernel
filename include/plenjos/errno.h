f0b20c5280154db3dd476d8f9eea328a8f6c33c0
plenjamin <53486418+benmontycomputer@users.noreply.github.com>
2025-12-20 02:03:00 -0500

    Kernel headers: add ENAMETOOLONG to errno

--- include/plenjos/errno.h
+++ include/plenjos/errno.h
@@ -8,7 +8,7 @@
  */
 
 // Required by C standard
-#define EDOM 39   // Device not managed
+#define EDOM   39 // Device not managed
 #define EILSEQ 42 // Illegal byte sequence
 #define ERANGE 34 // Result too large
 
@@ -19,13 +19,14 @@
 #define ENOSYS 38 // Function not implemented
 
 // I/O errors
-#define ENOENT 2   // No such file or directory
-#define EIO 5      // I/O error
-#define EBADF 9    // Bad file descriptor
-#define EACCES 13  // Permission denied
-#define EEXIST 17  // File exists
-#define ENOTDIR 20 // Not a directory
-#define EISDIR 21  // Is a directory
-#define EMFILE 24  // Too many open files
+#define ENOENT       2  // No such file or directory
+#define EIO          5  // I/O error
+#define EBADF        9  // Bad file descriptor
+#define EACCES       13 // Permission denied
+#define EEXIST       17 // File exists
+#define ENOTDIR      20 // Not a directory
+#define EISDIR       21 // Is a directory
+#define EMFILE       24 // Too many open files
+#define ENAMETOOLONG 36 // Filename too long
 
 #endif /* _PLENJOS_ERRNO_H */
